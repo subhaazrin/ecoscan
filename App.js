56d3c10c52bbd9dd3ff986338b61d2bf471f5332
Gabriel Tan-Chen <gabriel@tan-chen.com>
2022-03-09 20:59:35 -0500

    Call Clarifai API

--- App.js
+++ App.js
@@ -22,7 +22,6 @@ export default function App() {
 				<Stack.Screen name="Home" component={HomeScreen}/>
       			<Stack.Screen name="Scanner" component={EcoScannerScreen}/>
 				<Stack.Screen name="Camera" component={CameraScreen}/>
-				<Stack.Screen name="Results" component={ResultsScreen} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
@@ -74,60 +73,21 @@ resize = async photo => {
 	return manipulatedImage.base64;
 }
 
-predict = async image => {
-
-	let predictions = await clarifai.models.predict(
-	//predicts general MODE NOT FOOD
-	clarifai.GENERAL.MODEL, // model need to get prediction from
-	image
-	);
-	return predictions;
-};
-
-
 const CameraScreen = ({navigation, route}) => {
-	// const id = route.params.id;
 	const [type, setType] = useState(Camera.Constants.Type.back);
 	const [previewVisible, setPreviewVisible] = useState(false)
 	const [capturedImage, setCapturedImage] = useState(null)
-	//const [hasPermission, setHasPermission] = useState(null);
 
 	const { status } = Camera.requestCameraPermissionsAsync();
- /*
-	const __takePicture = async () => {
-	 const photo = await camera.takePictureAsync()
-	 console.log(photo)
-	 setPreviewVisible(true)
-	 // //setStartCamera(false)
-	 setCapturedImage(photo)
-	 return photo.uri;
- }*/
 
- objectDetection = async () => {
-
-	const photo = await camera.takePictureAsync()
-	console.log(photo)
-	 setPreviewVisible(true)
-	 // //setStartCamera(false)
-	 setCapturedImage(photo)
-	// let resized = await this.resize(photo);
-	// let predictions = await this.predict(resized);
-	// this.setState({ predictions: predictions.outputs[0].data.concepts });
-	// console.log(this.state.predictions);
-	return photo.uri;
-	//return predictions;
-
-}
-
- 	// const Clarifai = require('clarifai');
-	//
-	// const app  = new Clarifai.App({
-  //   apiKey: 'c5c1b443f2ed41248f644a7dbeeb71a4'
-	// })
-
-	// process.nextTick = setImmediate;
-
-
+	objectDetection = async () => {
+		// I added the base64 flag for takePictureAsync to return the bytes of the
+		// image in base64 format: https://docs.expo.dev/versions/latest/sdk/camera/#takepictureasync
+		const photo = await camera.takePictureAsync({ base64: true })
+		setPreviewVisible(true)
+		setCapturedImage(photo)
+		return photo.uri;
+	}
 
 	if (status === null) {
 		return <View />;
@@ -139,42 +99,36 @@ const CameraScreen = ({navigation, route}) => {
 
 	return (
 		<View style={styles.camera}>
-		{previewVisible && capturedImage ? (
-			<CameraPreview photo={capturedImage} navigation={navigation} />
-		) : (
-			<Camera style={styles.camera} type={type} ref={(r) => { camera = r }}>
-				<View style={styles.buttonContainer}>
-				<TouchableOpacity
-					style={styles.button}
-					onPress={() => {
-					setType(
-						type === Camera.Constants.Type.back
-						? Camera.Constants.Type.front
-						: Camera.Constants.Type.back
-					);
-					}}>
-					<Text style={styles.text}> Flip </Text>
-				</TouchableOpacity>
-				<TouchableOpacity
-					style={styles.button}
-					onPress={this.objectDetection}>
-					<Text style={styles.text}> Take Photo </Text>
-				</TouchableOpacity>
-				</View>
-			</Camera>
-		)}
+			{previewVisible && capturedImage ? (
+				<CameraPreview photo={capturedImage} navigation={navigation} />
+			) : (
+				<Camera style={styles.camera} type={type} ref={(r) => { camera = r }}>
+					<View style={styles.buttonContainer}>
+					<TouchableOpacity
+						style={styles.button}
+						onPress={() => {
+						setType(
+							type === Camera.Constants.Type.back
+							? Camera.Constants.Type.front
+							: Camera.Constants.Type.back
+						);
+						}}>
+						<Text style={styles.text}> Flip </Text>
+					</TouchableOpacity>
+					<TouchableOpacity
+						style={styles.button}
+						onPress={this.objectDetection}>
+						<Text style={styles.text}> Take Photo </Text>
+					</TouchableOpacity>
+					</View>
+				</Camera>
+			)}
 		</View>
-  	);
-
+  );
 }
 
-
 const CameraPreview = ({photo, navigation }) => {
-
-  console.log('sdsfds', photo)
-
-  //alert(predictions);
-  //var predict  = predictions
+	const [showResults, setShowResults] = useState(false);
 
   return (
     <View
@@ -185,44 +139,79 @@ const CameraPreview = ({photo, navigation }) => {
         height: '100%'
       }}
     >
-      <ImageBackground
-        source={{uri: photo && photo.uri}}
-        style={{
-          flex: 1
-        }}
-      />
-			<Button style={styles.button}
-				title="Go to results"
-				onPress={() => navigation.navigate('Results')}
-			/>
+			{showResults ? (
+				<ResultsScreen photo={photo} />
+			) : (
+				<>
+					<ImageBackground
+						source={{uri: photo && photo.uri}}
+						style={{
+							flex: 1
+						}}
+					/>
+					<Button style={styles.button}
+						title="Go to results"
+						onPress={() => setShowResults(true)}
+					/>
+				</>
+			)}
     </View>
-
-
   )
 }
 
-const ResultsScreen = ({navigation, photo}) => {
+const ResultsScreen = ({photo}) => {
+	const [prediction, setPrediction] = useState("Waiting for results...")
 
-	// objectDetection = async () => {
-	//
-	// 	let resized = await this.resize(photo);
-	// 	let predictions = await this.predict(resized);
-	// 	this.setState({ predictions: predictions.outputs[0].data.concepts });
-	// 	console.log(this.state.predictions);
-	//
-	// 	//return predictions;
-	//
-	// }
+	const raw = JSON.stringify({
+		"user_app_id": {
+			"user_id": "", // @todo Fill in your user_id
+			"app_id": "", // @todo Fill in your app_id
+		},
+		"inputs": [{
+			"data": {
+				"image": {
+					"base64": photo.base64,
+				},
+			},
+		}],
+	});
+	const requestOptions = {
+		method: "POST",
+		headers: {
+			"Accept": "application/json",
+			"Authorization": "Key YOUR_KEY_HERE", // @todo Fill in your API key
+		},
+		body: raw,
+	};
+
+	fetch("https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/versions/aa9ca48295b37401f8af92ad1af0d91d/outputs", requestOptions)
+		.then(response => response.text())
+		.then(r => {
+			let result = JSON.parse(r, null, 2)
+			console.log(result.outputs);
+			const output = result.outputs[0];
+			if (output && output.data && output.data.concepts) {
+				let resultText = "Predicted concepts:\n";
+				for (const concept of output.data.concepts) {
+					resultText += concept.name;
+					resultText += " ";
+					resultText += concept.value;
+					resultText += "\n";
+					setPrediction(resultText);
+				}
+			} else {
+				setPrediction("No predictions returned");
+			}
+		})
+		.catch(err => console.log('error', err))
 
 	return(
 		<View style={styles.container}>
 			<Text> Results:</Text>
-			<Text>Hello world</Text>
+			<Text>{prediction}</Text>
 		</View>
 	)
 }
-
-
 
 const styles = StyleSheet.create({
 	button: {flex: 1, alignItems: 'center', justifyContent: 'center'},
